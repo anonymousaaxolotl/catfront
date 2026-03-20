4ec32647a09d25c147efb69780d148389c5c3c6a
anonymousaaxolotl <a39479654@gmail.com>
2026-03-20 12:52:45 +0000

    Deploying to gh-pages from @ anonymousaaxolotl/catfront@27658e96e6170875e58c5152517a5e6e36dd010c 🚀

---
+++ assets/product-CynkW6Vx.js
@@ -0,0 +1,1 @@
+import{a as o}from"./api-Dlgn2r9h.js";const e=t=>o.get("/product",{params:t}),p=t=>o.post("/product",t),r=(t,c)=>o.patch("/product/"+t,c),s=t=>o.delete("/product/"+t),d={getAll:e,create:p,update:r,remove:s};export{d as p};
